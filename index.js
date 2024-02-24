6f3295793919880ea23a7556211a931672ff646a
unknown <shinchanmusic24@gmail.com>
2024-02-24 09:49:29 +0530

    add employee function

--- index.js
+++ index.js
@@ -8,8 +8,43 @@
 
     const employeeList = document.querySelector(".employee_names");
     const employeeSingleInfo = document.querySelector(".employee_info_single");
+    const createEmployee = document.querySelector(".createEmployee");
+    const addEmployeeModal = document.querySelector(".addEmployee");
+    const addEmployeeForm = document.querySelector(".addEmployee_create");
 
     //Add Employee Logic
+    createEmployee.addEventListener("click",()=>{
+        addEmployeeModal.style.display = "flex";
+    });
+    addEmployeeModal.addEventListener("click",(e)=>{
+        if(e.target.className==="addEmployee"){
+            addEmployeeModal.style.display="none";
+        }
+    })
+    const dobInput = document.querySelector(".addEmployee_create--dob");
+    dobInput.max = `${new Date().getFullYear() - 18}-${new Date().toISOString().slice(5, 10)}` 
+
+    addEmployeeForm.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        const formData = new FormData(addEmployeeForm);
+        const values = [...formData.entries()];
+        console.log(values);
+        let empData ={};
+        values.forEach((val)=>{
+            empData[val[0]] = val[1];
+        })
+        empData.id = employees[employees.length-1].id +1;
+       
+            empData.age = new Date().getFullYear() - parseInt(empData.dob.slice(0, 4), 10);
+        
+        empData.imageUrl = empData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png";
+        console.log(empData)
+        employees.push(empData);
+        renderEmployee();
+        addEmployeeForm.reset();
+        addEmployeeModal.style.display = "none";
+
+    })
 
     //Select EMployee
     employeeList.addEventListener("click", (e) => {
